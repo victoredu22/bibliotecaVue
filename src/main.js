adcfff8b15787a5f8fcebac24586b59bd8fd10d8
victor <victor.edu22@gmail.com>
2021-04-17 14:42:45 -0400

    change head

--- src/main.js
+++ src/main.js
@@ -36,6 +36,7 @@ Vue.use(ServerTable,{},false, 'bootstrap4','default');
 //axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.baseURL = 'http://104.236.211.192';
 
+
 /* 
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
