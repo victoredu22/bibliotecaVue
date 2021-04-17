18a62ece16e3d523aa81e46ca2c3acdfa7b0933f
victor <victor.edu22@gmail.com>
2021-04-17 14:08:27 -0400

    save env

--- src/main.js
+++ src/main.js
@@ -33,8 +33,8 @@ Vue.use(ServerTable,{},false, 'bootstrap4','default');
 
 
 // Agregamos la URL base de nuestra API
-axios.defaults.baseURL = 'http://localhost:8000';
-//axios.defaults.baseURL = 'https://victor-api2021.herokuapp.com';
+//axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'http://104.236.211.192';
 
 /* 
 import VueSidebarMenu from 'vue-sidebar-menu'
